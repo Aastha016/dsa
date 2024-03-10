b4c59d52d857821714b50694dad62292134f8db4
Aastha016 <aasthakapoor016@gmail.com>
2024-03-11 00:03:32 +0530

    Permutation v2 with set to reduce TC

--- Backtracking/Permutations.java
+++ Backtracking/Permutations.java
@@ -1,7 +1,18 @@
 package Backtracking;
 
+
+import java.util.HashSet;
+
+/*
+* the time complexity of list.contains is O(n) therefore to reduce that
+* we can use a set becoz Tc of contains in set is O(1) and not O(n)
+*
+*
+*
+* */
 public class Permutations {
     List<List<Integer>> result = new ArrayList<>();
+    Set<Integer> st = new HashSet<Integer>();
     public List<List<Integer>> permute(int[] nums) {
         List<Integer> temp = new ArrayList<>();
         solve(nums,temp);
@@ -14,10 +25,12 @@ public class Permutations {
             return;
         }
         for(int i =0;i<nums.length;i++){
-            if(!temp.contains(nums[i])){
+            if(!st.contains(nums[i])){
                 temp.add(nums[i]);
+                st.add(nums[i]);
                 solve(nums,temp);
                 temp.remove(temp.size()-1);
+                st.remove(nums[i]);
                 // solve(nums,temp);
             }
         }
