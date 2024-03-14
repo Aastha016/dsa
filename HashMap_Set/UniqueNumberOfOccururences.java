39c9fc87120f8524b87533f06839f6d10dcd5fd5
Aastha016 <aasthakapoor016@gmail.com>
2024-03-15 02:11:02 +0530

    Q2 hashmap/set approach 2

--- HashMap_Set/UniqueNumberOfOccururences.java
+++ HashMap_Set/UniqueNumberOfOccururences.java
@@ -17,3 +17,27 @@ public class UniqueNumberOfOccururences {
         return true;
     }
 }
+
+
+
+--------------------------------------------------------------------------
+//Approach 2 without auxilary space
+
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        int a  [] = new int [2001];
+        Arrays.fill(a,0);
+
+        for(int i =0;i<arr.length;i++){
+            int num = 1000+arr[i];
+            a[num]++;
+        }
+        Arrays.sort(a);
+
+        for(int i =1;i<2001;i++){
+            if(a[i]!=0 && a[i]==a[i-1])
+                return false;
+        }
+        return true;
+
+    }
